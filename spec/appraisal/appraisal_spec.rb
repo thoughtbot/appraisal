8023273a764bbeec0ddb068c97e8080dabe0d398
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2012-03-18 03:11:39 -0400

    add test for bundle command creation

---
+++ spec/appraisal/appraisal_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'appraisal/appraisal'
+
+describe Appraisal::Appraisal do
+  it "creates a proper bundle command" do
+    appraisal = Appraisal::Appraisal.new('fake', 'fake')
+    appraisal.stub(:gemfile_path)
+      .and_return("/home/test/test directory")
+
+    appraisal.bundle_command.should == "bundle install --gemfile='/home/test/test directory'"
+  end
+end
