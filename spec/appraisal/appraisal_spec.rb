c7e8197d828fe9666addb7df5b44c9d08aec0e80
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-03-25 15:14:51 -0400

    Fix syntax error.

--- spec/appraisal/appraisal_spec.rb
+++ spec/appraisal/appraisal_spec.rb
@@ -4,8 +4,7 @@ require 'appraisal/appraisal'
 describe Appraisal::Appraisal do
   it "creates a proper bundle command" do
     appraisal = Appraisal::Appraisal.new('fake', 'fake')
-    appraisal.stub(:gemfile_path)
-      .and_return("/home/test/test directory")
+    appraisal.stub(:gemfile_path).and_return("/home/test/test directory")
 
     appraisal.bundle_command.should == "bundle install --gemfile='/home/test/test directory'"
   end
