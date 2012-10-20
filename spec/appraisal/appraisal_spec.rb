ec1caf1a197973eff51923d59044a46a358c2143
grosser <grosser.michael@gmail.com>
2012-10-20 12:20:44 -0700

    check and only install if necessary -> faster

--- spec/appraisal/appraisal_spec.rb
+++ spec/appraisal/appraisal_spec.rb
@@ -6,7 +6,7 @@ describe Appraisal::Appraisal do
     appraisal = Appraisal::Appraisal.new('fake', 'fake')
     appraisal.stub(:gemfile_path).and_return("/home/test/test directory")
 
-    appraisal.bundle_command.should == "bundle install --gemfile='/home/test/test directory'"
+    appraisal.bundle_command.should == "bundle check --gemfile='/home/test/test directory' || bundle install --gemfile='/home/test/test directory'"
   end
 
   it "cleans up spaces and punctuation when outputting its gemfile" do
