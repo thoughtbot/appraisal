33775b4ca7cc92cdfd07bd8f943d5c15478b4a2f
Prem Sichanugrist <s@sikac.hu>
2014-02-28 12:43:53 -0500

    Fix build error on Rubinius by requiring matcher
    There was some problem with class autoloading in Rubinius, which can be
    workaround by just requiring that missing file.
    
    See https://github.com/rubinius/rubinius/issues/2934

--- spec/appraisal/file_spec.rb
+++ spec/appraisal/file_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 require 'appraisal/file'
 
+# Requiring this to make the test pass on Rubinius 2.2.5
+# https://github.com/rubinius/rubinius/issues/2934
+require 'rspec/matchers/built_in/raise_error'
+
 describe Appraisal::File do
   it "should complain when no Appraisals file is found" do
     ::File.stub(:exist?).with("Appraisals").and_return(false)
