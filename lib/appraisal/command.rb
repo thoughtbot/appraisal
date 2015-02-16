a6f45796e251aa848a5a5106bcc801a07444ded6
Elliot Winkler <elliot.winkler@gmail.com>
2015-02-16 11:06:50 -0700

    Allow commands with spaces to be constructed as new Commands
    If you were using the `appraisal` executable and passed an argument that
    contained spaces, those spaces would not be preserved when Appraisal
    went to execute the full command. For instance, this command...
    
        appraisal 4.2 rspec spec/some/file_spec.rb -e "some example group"
    
    *Should* be expanded as follows:
    
        BUNDLE_GEMFILE=$PWD/gemfiles/4.2.gemfile rspec spec/some/file_spec.rb -e "some example group"
    
    ...but before this commit would be incorrectly expanded as follows:
    
        BUNDLE_GEMFILE=$PWD/gemfiles/4.2.gemfile rspec spec/some/file_spec.rb -e some example group
    
    Close #86

--- lib/appraisal/command.rb
+++ lib/appraisal/command.rb
@@ -1,28 +1,23 @@
+require "shellwords"
+
 module Appraisal
   # Executes commands with a clean environment
   class Command
     BUNDLER_ENV_VARS = %w(RUBYOPT BUNDLE_PATH BUNDLE_BIN_PATH BUNDLE_GEMFILE).freeze
 
-    def self.from_args(gemfile)
-      ARGV.shift
-      command = ([$0] + ARGV).join(' ')
-      new(command, gemfile)
-    end
+    attr_reader :command, :env, :gemfile, :original_env
 
-    def initialize(command, gemfile = nil)
+    def initialize(command, options = {})
+      @gemfile = options[:gemfile]
+      @env = options.fetch(:env, {})
+      @command = command_starting_with_bundle(command)
       @original_env = {}
-      @gemfile = gemfile
-      if command =~ /^(bundle|BUNDLE_GEMFILE)/
-        @command = command
-      else
-        @command = "bundle exec #{command}"
-      end
     end
 
     def run
       announce
       with_clean_env do
-        unless Kernel.system(@command)
+        unless Kernel.system(env, command_as_string)
           exit(1)
         end
       end
@@ -30,14 +25,14 @@ module Appraisal
 
     def exec
       announce
-      with_clean_env { Kernel.exec(@command) }
+      with_clean_env { Kernel.exec(env, command_as_string) }
     end
 
     private
 
     def with_clean_env
       unset_bundler_env_vars
-      ENV['BUNDLE_GEMFILE'] = @gemfile
+      ENV['BUNDLE_GEMFILE'] = gemfile
       ENV['APPRAISAL_INITIALIZED'] = '1'
       yield
     ensure
@@ -45,22 +40,46 @@ module Appraisal
     end
 
     def announce
-      if @gemfile
-        puts ">> BUNDLE_GEMFILE=#{@gemfile} #{@command}"
+      if gemfile
+        puts ">> BUNDLE_GEMFILE=#{gemfile} #{command_as_string}"
       else
-        puts ">> #{@command}"
+        puts ">> #{command_as_string}"
       end
     end
 
     def unset_bundler_env_vars
       BUNDLER_ENV_VARS.each do |key|
-        @original_env[key] = ENV[key]
+        original_env[key] = ENV[key]
         ENV[key] = nil
       end
     end
 
     def restore_env
-      @original_env.each { |key, value| ENV[key] = value }
+      original_env.each { |key, value| ENV[key] = value }
+    end
+
+    def command_starts_with_bundle?(original_command)
+      if original_command.is_a?(Array)
+        original_command.first =~ /^bundle/
+      else
+        original_command =~ /^bundle/
+      end
+    end
+
+    def command_starting_with_bundle(original_command)
+      if command_starts_with_bundle?(original_command)
+        original_command
+      else
+        %w(bundle exec) + original_command
+      end
+    end
+
+    def command_as_string
+      if command.is_a?(Array)
+        Shellwords.join(command)
+      else
+        command
+      end
     end
   end
 end
