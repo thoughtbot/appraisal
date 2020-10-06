59afec203715bbf8511b24c707b159fa45874a9a
James Ebentier <jebentier@invoca.com>
2020-10-06 18:35:47 +0200

    Break up build gems into multiple lins for readability

--- spec/acceptance/appraisals_file_bundler_dsl_compatibility_spec.rb
+++ spec/acceptance/appraisals_file_bundler_dsl_compatibility_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe 'Appraisals file Bundler DSL compatibility' do
   it 'supports all Bundler DSL in Appraisals file' do
-    build_gems %w(bagel orange_juice milk waffle coffee ham sausage pancake rotten_egg)
+    build_gems %w(bagel orange_juice milk waffle coffee ham
+      sausage pancake rotten_egg)
     build_git_gems %w(egg croissant pain_au_chocolat omelette)
 
     build_gemfile <<-Gemfile
