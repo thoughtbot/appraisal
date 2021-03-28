thoughtbot/appraisal

commit 586864393e405a67b1457b563a4d5adc99e50e2d

Auto-install the right bundler version (#168)

lib/appraisal/command.rb
@@ -41,9 +41,9 @@ module Appraisal
     end
 
     def ensure_bundler_is_available
-      unless system %(gem list -q "^bundler$" | grep -q bundler)
+      version = Utils.bundler_version
+      unless system %(gem list -i bundler -v #{version})
         puts ">> Reinstall Bundler into #{ENV["GEM_HOME"]}"
-        version = Utils.bundler_version
 
         unless system "gem install bundler --version #{version}"
           puts

.travis.yml
@@ -1,5 +1,5 @@
 ---
-before_install: gem install bundler:1.17.3
+before_install: gem install bundler:2.1.4
 
 rvm:
   - 2.3

commit ab41372e686ef8a23b14304f9e125a0b3bce2fb7

Silence `gem list` command output
Prevents `appraisal rake` to write a boolean on the standard output when
checking the installed version of bundler

Bug introduced in #168 586864393e405a67b1457b563a4d5adc99e50e2d

Fix: #180

lib/appraisal/command.rb
@@ -42,7 +42,7 @@ module Appraisal
 
     def ensure_bundler_is_available
       version = Utils.bundler_version
-      unless system %(gem list -i bundler -v #{version})
+      unless system %(gem list --silent -i bundler -v #{version})
         puts ">> Reinstall Bundler into #{ENV["GEM_HOME"]}"
 
         unless system "gem install bundler --version #{version}"