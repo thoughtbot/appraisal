5510b2996775f2e09c8ef0e78768f31d4ad30160
joe-sharp <joe.sharp@procore.com>
2022-03-25 14:46:12 -0500

    Shorten line and disambiguate block

--- spec/appraisal/customize_spec.rb
+++ spec/appraisal/customize_spec.rb
@@ -5,7 +5,9 @@ describe Appraisal::Customize do
   it "has defaults" do
     expect(described_class.heading).to eq nil
     expect(described_class.single_quotes).to eq false
-    expect { described_class.new }.to_not change { [described_class.heading, described_class.single_quotes] }
+    expect { described_class.new }.to_not(change do
+      [described_class.heading, described_class.single_quotes]
+    end)
   end
 
   it "can override defaults" do
