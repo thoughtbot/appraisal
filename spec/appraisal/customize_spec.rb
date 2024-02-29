86db4b8062fc46eececae3d2238aea5b205ed912
joe-sharp <joe.sharp@procore.com>
2024-02-29 02:14:39 -0600

    Fix spec for new Customize feature

--- spec/appraisal/customize_spec.rb
+++ spec/appraisal/customize_spec.rb
@@ -1,18 +1,21 @@
 require "spec_helper"
+require "appraisal/appraisal"
 require "appraisal/customize"
 
 describe Appraisal::Customize do
+  let(:appraisal) { Appraisal::Appraisal.new("test", "Gemfile") }
+
   it "has defaults" do
-    expect(described_class.heading).to eq nil
+    expect(described_class.heading(appraisal)).to eq nil
     expect(described_class.single_quotes).to eq false
     expect { described_class.new }.to_not(change do
-      [described_class.heading, described_class.single_quotes]
+      [described_class.heading(appraisal), described_class.single_quotes]
     end)
   end
 
   it "can override defaults" do
     described_class.new(single_quotes: true, heading: "foo")
-    expect(described_class.heading).to eq "foo"
+    expect(described_class.heading(appraisal)).to eq "foo"
     expect(described_class.single_quotes).to eq true
   end
 end
