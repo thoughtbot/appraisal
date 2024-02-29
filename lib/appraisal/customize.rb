7113812139fae36949879281d6ccc1e76ac15a48
joe-sharp <joe.sharp@procore.com>
2024-02-29 01:12:56 -0600

    Allow custom gemfile vars in Customize

--- lib/appraisal/customize.rb
+++ lib/appraisal/customize.rb
@@ -5,12 +5,26 @@ module Appraisal
       @@single_quotes = single_quotes
     end
 
-    def self.heading
+    def self.heading(gemfile)
       @@heading ||= nil
+      customize(@@heading, gemfile)
     end
 
     def self.single_quotes
       @@single_quotes ||= false
     end
+
+    def self.customize(heading, gemfile)
+      return nil unless heading
+
+      format(
+        heading.to_s,
+        gemfile: gemfile.send("gemfile_name"),
+        gemfile_path: gemfile.gemfile_path,
+        relative_gemfile_path: gemfile.relative_gemfile_path
+      )
+    end
+
+    private_class_method :customize
   end
 end
